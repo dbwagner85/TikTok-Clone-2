5c2af3f81d1397d851641270ce688eb9b27e7ad6
dbwagner85 <dbwagner@my.waketech.edu>
2022-07-03 14:11:26 -0400

    TikTok Clone
    WEB 215 TikTok Clone

---
+++ tiktok-backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url: "https://player.vimeo.com/external/413882311.sd.mp4?s=c720a6b81450133a70f3034e44dce9d5316c0c04&profile_id=165&oauth2_token_id=57447761",
+        channel: "devincodes",
+        song: "99 problems but REACT AINT ONE!",
+        likes: "123",
+        messages: "456",
+        description: "YOOO this works",
+        shares: "789",
+    },
+    {
+        url: "https://player.vimeo.com/external/500947126.sd.mp4?s=99d64b1d65b5e35586f5fc7d84ccd7ae04a9ccad&profile_id=165&oauth2_token_id=57447761",
+        channel: "dbwagner",
+        description: "Still Workin!",
+        song: "My REACT Song",
+        likes: "987",
+        messages: "654",
+        shares: "321",
+    },
+];
+
+export default data;
---
+++ tiktok/build/precache-manifest.aac4585129d919e9eec36adeeb89fb34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb44f027cf6967f6efb0",
+    "url": "/static/css/main.017f6089.chunk.css"
+  },
+  {
+    "revision": "bb44f027cf6967f6efb0",
+    "url": "/static/js/main.bb44f027.chunk.js"
+  },
+  {
+    "revision": "9d6d0ad7f97bf131636f",
+    "url": "/static/js/1.9d6d0ad7.chunk.js"
+  },
+  {
+    "revision": "4158b28fda0c97eacfe4",
+    "url": "/static/js/2.4158b28f.chunk.js"
+  },
+  {
+    "revision": "5ebfd4f2ddfb36e0fc17",
+    "url": "/static/js/runtime~main.5ebfd4f2.js"
+  },
+  {
+    "revision": "15e39e5a8184c7781240ff70b3e89e14",
+    "url": "/index.html"
+  }
+];
